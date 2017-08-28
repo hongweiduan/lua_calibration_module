ef96082767a6ba923f28d284361cb26bbaeaa063
Cassie <18370567571@163.com>
2017-08-28 16:54:45 +0800

    添加系统测试的参数

--- config.lua
+++ config.lua
@@ -1,13 +1,29 @@
 ----all same----
-work_path 								= '/Users/Howie/Documents/GIT_Source/lua_calibration_module/'
-log_path								= '/vault/Intelli_L/CAL_LOG/'
+work_path 								= '/Users/bill/Desktop/lua_calibration_module-master/'
+log_path								= '/vault/Intelli_Log/CAL_LOG/'
 ---for test----
 test_data_before_cal_path 				= work_path..'test/test_data_before_cal.csv'
 test_data_after_cal_path 				= work_path..'test/test_data_after_cal.csv'
-factor_path                             = work_path..'test/factor.txt'
 
+---different for board----
+interface_name 							= 'interface'
+board_name								= 'Dead_Time_Relay_Board'
+board_sn								= 'IN569EVTDT12345678'
 
+precision_percent						= 0.0015
+precision_deviance						= 1
+k_limit									= {0.95,1.05}					
 
+step_unit								= 'mV'
+meas_unit								= 'mV'
+
+tab1      								= {1,6,1}
+station_name							= '017'
+channel_name							= 's1'
+system_id								= 'IN569'
+boardOrsystem							= 'system'
+-- file_name1								= 'cal'
+-- file_name2								= 'check'
 
 
 
