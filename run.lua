d9caa39d50b974af4e1686ea042543e7c4aba3dd
Cassie <18370567571@163.com>
2017-08-28 16:51:36 +0800

    run.la
    修改了调用函数的参数

--- run.lua
+++ run.lua
@@ -1,20 +1,10 @@
 cal = require("calibration")
 require "common"
---[[
 setting_name = "interface.interface_1"
 saveCfgToFile("cal_config",setting_name)
-cal_tab = {1,6,1}
-cal.cal_with_head(cal_tab)
-
+cal.cal_with_head("cal")
+-- cal.check_with_head()
 
 setting_name = "interface.interface_2"
 saveCfgToFile("cal_config",setting_name)
-check_tab = {7,12,1}
-cal.cal_without_head(check_tab)
---]]
-setting_name = "interface"
-saveCfgToFile("cal_config",setting_name)
-cal_coeff_address="0x01"
-cal_tab = {1,6,1}
-cal.cal_with_head(cal_tab)
--- print_name()
+cal.check_with_head('check')
